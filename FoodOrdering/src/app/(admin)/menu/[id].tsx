421557fda63cc866d85cf61c46c48bc034417f85
VishnuKartha <vishnukartha1@gmail.com>
2024-08-30 00:46:13 -0700

    add add and delete ui

--- FoodOrdering/src/app/(admin)/menu/[id].tsx
+++ FoodOrdering/src/app/(admin)/menu/[id].tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, View, Text, Image, Pressable } from "react-native";
 import React from "react";
-import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 import products from "@assets/data/products";
 import { defaultPizzaImage } from "@components/ProductListItem";
 import { useState } from "react";
 import Button from "@components/Button";
 import { useCart } from "@/src/providers/CartProvider";
 import { PizzaSize } from "@/src/types";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/src/constants/Colors";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 const ProudctDetailsScreen = () => {
@@ -31,6 +33,25 @@ const ProudctDetailsScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
       <Stack.Screen options={{ title: product?.name }} />
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
